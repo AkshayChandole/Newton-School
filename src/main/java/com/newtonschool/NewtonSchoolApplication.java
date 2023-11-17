326dd678aad7c38927470319fb7c7180a6495005
AkshayChandole <akshaychandole1234@gmail.com>
2023-11-17 19:52:18 +0530

    Feature - home page for newton school application

---
+++ src/main/java/com/newtonschool/NewtonSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.newtonschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewtonSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewtonSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newtonschool/NewtonSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newtonschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewtonSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
